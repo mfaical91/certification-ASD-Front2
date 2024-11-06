dd1fe13bc18d71abda094170f7769ea61d1e6796
faical <mnafegfaical@gmail.com>
2024-11-06 16:31:57 +0100

    documentation sur l'instalation de Apache sous windows64

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+// Sélection des liens de navigation
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
